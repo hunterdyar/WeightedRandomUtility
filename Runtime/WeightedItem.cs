16f5197993a00dc7989503a979172eadbdc3b0ad
hunterdyar <bloop@hdyar.com>
2024-04-05 23:35:48 -0400

    Initial

---
+++ Runtime/WeightedItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace HDyar.WeightedRandomUtility
+{
+	[System.Serializable]
+	public class WeightedItem<T>
+	{
+		public T item;
+		[Min(0)] public float weight = 0;
+		public AnimationCurve Curve = AnimationCurve.Linear(0,0,1,1);
+		public void SetWeightByCurve(float x)
+		{
+			weight = Curve.Evaluate(x);
+		}
+	}
+}
---
+++ Runtime/WeightedSelection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace HDyar.WeightedRandomUtility
+{
+	[System.Serializable]
+	public class WeightedSelection<T>
+	{
+		public List<WeightedItem<T>> Items = new List<WeightedItem<T>>();
+		public int Count => Items.Count;
+
+		public WeightedItem<T> GetWeightedItem(T key)
+		{
+			return Items.Find(x => x.item.Equals(key));
+		}
+
+		public bool TryGetWeightedItem(T key, out WeightedItem<T> item)
+		{
+			item = Items.Find(x => x.item.Equals(key));
+			if (item != null)
+			{
+				return true;
+			}
+
+			return false;
+		}
+
+		public void SetAllItemWeightsByCurve(float x)
+		{
+			foreach (var item in Items)
+			{
+				item.SetWeightByCurve(x);
+			}
+		}
+
+		public WeightedItem<T> GetWeightedItemByIndex(int index)
+		{
+			return Items[index];
+		}
+
+		public T GetWeightedRandomItem()
+		{
+			if (Items.Count == 0)
+			{
+				throw new IndexOutOfRangeException("Can't get random item from empty selection.");
+				return default;
+			}
+
+			float total = Items.Sum(x => x.weight);
+			float random = UnityEngine.Random.Range(0, total);
+
+			float running = 0;
+			for (int i = 0; i < Items.Count; i++)
+			{
+				running += Items[i].weight;
+				if (random <= running)
+				{
+					return Items[i].item;
+				}
+			}
+
+			throw new IndexOutOfRangeException("Unable to select random item. Are all weights valid values?");
+		}
+	}
+}
